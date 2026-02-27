d3b690f2d7246200127f152d727e509d034dfb60
Abraham <abkuri88@gmail.com>
2026-02-26 23:58:44 -0600

    Pin Stimulus modules in importmap for design system entrypoint

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from UntitledUi::Engine.root.join("app/javascript/untitled_ui"),
              under: "untitled_ui",
              to: "untitled_ui"
